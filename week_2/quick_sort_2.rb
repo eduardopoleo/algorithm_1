fe6aaf2986eb4aac52082be3f1616eb6c934699d
Eduardo Poleo <dev+eduardo@influitive.com>
2016-09-15 23:15:00 -0400

    Counters now make a lot more sense

--- week_2/quick_sort_2.rb
+++ week_2/quick_sort_2.rb
@@ -1,33 +1,34 @@
 def quick_sort_2(a, lo, hi)
-  # there can't be iteration if lo == hi
-  return a if lo == hi
+  # if at some point everyting will be sorted and nothing will get moved and m - 1 = 0
+  # when this happens lo = 0, i -1 = -1
+  # this obviously is not right can be used for iteration
+  # it's better to leave this condition till the end to reason about it
+  return a if lo > hi
 
   # i is the position of the element right in front of the pivot
   i = linear_partitioning(a, lo, hi)
 
-  quick_sort_2(a, lo, i - 1)
-  quick_sort_2(a, i, hi)
+  quick_sort_2(a, lo, i - 1 )
+  quick_sort_2(a, i + 1, hi)
 end
 
-
 def linear_partitioning(a, lo, hi)
-  pivot_index = rand(lo...hi)
+  pivot_index = rand(lo..hi)
   pivot = a[pivot_index]
   swap(a, pivot_index, lo)
 
-  #i by definition is always one extra place than the pivot
-  i = lo + 1
+  m = lo + 1
 
-  for j in lo...hi
+  for j in lo..hi
     if a[j] < pivot
-      swap(a, j, i)
-      i += 1
+      swap(a, j, m)
+      m += 1
     end
   end
 
-  swap(a, i-1, lo)
+  swap(a, m - 1, lo)
 
-  i
+  m - 1
 end
 
 def swap(a, i, j)
@@ -40,4 +41,4 @@ end
 
 a = [3,8,2,5,1,4,7,6]
 
-p quick_sort_2(a, 0, 8)
+p quick_sort_2(a, 0, 7)
