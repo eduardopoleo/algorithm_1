321864ce327b4a35b8a8b61b53616eec1a06b8ef
Eduardo Poleo <dev+eduardo@influitive.com>
2016-09-15 20:11:29 -0400

    linear partitioning working, quicksort broken

---
+++ week_2/master_method.rb
@@ -0,0 +1,15 @@
+def binary_search(a)
+
+  return a if a.lentgh == 1
+  half1
+  half2
+
+  return half1[-1] if == a
+  return half2[-1] if == a
+
+  if half1[-1] > b
+    binary_search(half1)
+  else
+    binary_search(half2)
+  end
+end
---
+++ week_2/quick_sort.rb
@@ -0,0 +1,38 @@
+#high overview
+
+a = [3,8,2,5,1,4,7,6]
+require 'pry'
+
+def quick_sort(a, lo, hi)
+  return a if lo >= hi
+
+  i = linear_partitioning(a, lo, hi)
+
+  quick_sort(a, lo, i-1)
+  quick_sort(a, i+1, hi)
+  # This works because the changes are done in place and there is no need to
+  # merge the arrays into one.
+end
+
+def linear_partitioning(a, lo, hi)
+  pivot = a[lo]
+  i = lo + 1
+  for j in lo...hi
+    if a[j] < pivot
+      element_to_swap = a[i]
+      a[i] = a[j]
+      a[j] = element_to_swap
+      i += 1
+    end
+  end
+
+  element_to_swap = a[i - 1] # I gotta remember that this is just a swap nothing else
+  a[i - 1] = a[lo]
+  a[lo] = element_to_swap
+
+  a
+end
+
+sorted_array = linear_partitioning(a, 0, 8)
+
+p sorted_array
