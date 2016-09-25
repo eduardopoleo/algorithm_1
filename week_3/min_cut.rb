5a4f375c8640809474957ca23b54c485a72bb614
Eduardo Poleo <dev+eduardo@influitive.com>
2016-09-24 21:33:36 -0400

    broken nil stupid thing

---
+++ week_3/min_cut.rb
@@ -0,0 +1,97 @@
+require 'pry'
+
+class Vertex
+  attr_accessor :own_vertices, :adjecent_vertices
+
+  def initialize(initial_node_id, adjecent_vertices)
+    @own_vertices = initial_node_id
+    @adjecent_vertices = adjecent_vertices
+  end
+
+  def merge(other_vertex)
+    self.adjecent_vertices = remove_redundant_edges(other_vertex)
+    self.own_vertices = merge_own(other_vertex)
+    self.adjecent_vertices = merge_adjecent(other_vertex)
+  end
+
+  private
+
+  def remove_redundant_edges(other_vertex)
+    self.adjecent_vertices - other_vertex.own_vertices
+  end
+
+  def merge_own(other_vertex)
+    self.own_vertices + other_vertex.own_vertices
+  end
+
+  def merge_adjecent(other_vertex)
+    # This allows for parallel edges
+    self.adjecent_vertices + other_vertex.adjecent_vertices
+  end
+end
+
+class Graph
+  attr_accessor :vertices
+
+  def initialize(vertices)
+    @vertices = vertices
+  end
+
+  def pick_edge
+    vertex1, edge = calculate_contraction_point
+    vertex2, v2_index = find_vertex(edge)
+
+    [vertex1, vertex2, v2_index]
+  end
+
+  def contract(vertex1, vertex2, v2_index)
+    vertex1.merge(vertex2)
+    vertices.delete_at(v2_index)
+  end
+
+  def calculate_min_cut
+    vertices.first.adjecent_vertices.length
+  end
+
+  def size
+    vertices.length
+  end
+
+  def find_vertex(edge)
+    vertex_index = vertices.index{|v| v.own_vertices.include?(edge)}
+    [vertices[vertex_index], vertex_index]
+  end
+
+  private
+
+  def calculate_contraction_point
+    vertex_index = rand(0...vertices.length)
+    vertex1 = vertices[vertex_index]
+    edge_index = rand(0...vertex1.adjecent_vertices.length)
+    edge = vertex1.adjecent_vertices[edge_index]
+
+    [vertex1, edge]
+  end
+
+end
+#
+# file = File.open('data_set.txt', 'r')
+# lines = file.readlines
+# file.close
+#
+# vertices = []
+# lines.each do |l|
+#   l.chomp!
+#   line = l.split("\t")
+#   vertex = Vertex.new(line.slice(0...1), line.slice(1..-1))
+#   vertices << vertex
+# end
+#
+# graph = Graph.new(vertices)
+#
+# while graph.size > 2 do
+#   vertex1, vertex2, v2_index = graph.pick_edge
+#   graph.contract(vertex1, vertex2, v2_index)
+# end
+#
+# graph.calculate_min_cut
---
+++ week_3/min_cut_spec.rb
@@ -0,0 +1,41 @@
+require 'rspec'
+require_relative './min_cut'
+
+describe Vertex do
+  describe '#merge' do
+    let(:vertex1) { described_class.new([1,2,3], [4,5,6,7]) }
+    let(:vertex2) { described_class.new([4,5,6], [7,8,9]) }
+    let(:vertex3) { described_class.new([7,8,9], [1,2,4]) }
+
+    before do
+      vertex1.merge(vertex2)
+    end
+
+    it 'merges the own_vertives of both vertices' do
+      expect(vertex1.own_vertices).to eq([1,2,3,4,5,6])
+    end
+
+    it 'remove redundant adjacent_vertices and adds the new adjecent' do
+      expect(vertex1.adjecent_vertices).to match_array([7,7,8,9])
+    end
+  end
+end
+
+
+describe Graph do
+  let(:vertex1) { Vertex.new([1,2,3], [4,5,6,7]) }
+  let(:vertex2) { Vertex.new([4,5,6], [7,8,9]) }
+  let(:vertex3) { Vertex.new([7,8,9], [1,2,4]) }
+
+  before do
+    vertices = [vertex1, vertex2, vertex3]
+    @graph = Graph.new(vertices)
+    # @graph.contract(vertex1, vertex2, 1)
+  end
+
+  describe 'find_vertex' do
+    fit '' do
+      expect(@graph.find_vertex(7)).to eq([vertex3, 2])
+    end
+  end
+end
