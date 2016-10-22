e777475cfadf633c390131bc381ecebbe5f7905e
Eduardo Poleo <dev+eduardo@influitive.com>
2016-10-22 18:56:25 -0400

    Pass all test cases yihi

--- week_4/final_exercise.rb
+++ week_4/final_exercise.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 time1 = Time.now
 
-file = File.open('test_data_set3.txt', 'r')
+file = File.open('test_data_set5.txt', 'r')
 lines = file.readlines
 file.close
 
@@ -17,12 +17,21 @@ lines.each do |l|
   l.chomp!
   line = l.gsub("\n", "").split(" ")
 
+  # creates vertex that will hold the edge
   vertex = $vertices[line[0]]
   vertex ? vertex[:edges] << line[1] : $vertices[line[0]] = { seen: false, edges: [line[1]] }
+
+  # creates the edge node
+
+  edge = $vertices[line[1]]
+  $vertices[line[1]] = { seen: false, edges: [] } unless edge
 end	
 
 
 $vertices.each do |vertex_id, info|
+	vertex = $vertices_reverse[vertex_id]
+  $vertices_reverse[vertex_id] = { seen: false, edges: [] } unless vertex
+
 	info[:edges].each do |edge|
 		reverse_vertex = $vertices_reverse[edge]
 
@@ -64,7 +73,7 @@ end
 # Pass vertex_id so that I do not have to repeat the look up
 def dfs_visit_sort(vertex, vertex_id)
 	mark_seen(vertex)
-	
+
 	vertex[:edges].each do |e|
 		edge = $vertices[e]
 
