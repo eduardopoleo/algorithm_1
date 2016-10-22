0480576e73a3955cb2d7b74a83f29987ebded899
Eduardo Poleo <dev+eduardo@influitive.com>
2016-10-22 18:39:51 -0400

    works for simple graphs

--- week_4/final_exercise.rb
+++ week_4/final_exercise.rb
@@ -3,69 +3,94 @@ require 'pry'
 
 time1 = Time.now
 
-file = File.open('data_set.txt', 'r')
+file = File.open('test_data_set3.txt', 'r')
 lines = file.readlines
 file.close
 
-vertices = { }
-count = 0
+$vertices = { }
+$vertices_reverse = { }
+$sort = []
+$scc = []
+$all_scc = []
 
 lines.each do |l|
   l.chomp!
   line = l.gsub("\n", "").split(" ")
 
-  vertex = vertices[line[0]]
-  vertex ? vertex[:edges] << line[1] : vertices[line[0]] = { seen: false, edges: [line[1]] }
-
-	# count += 1
+  vertex = $vertices[line[0]]
+  vertex ? vertex[:edges] << line[1] : $vertices[line[0]] = { seen: false, edges: [line[1]] }
 end	
 
-vertices_reverse = { }
 
-count = 0
-vertices.each do |vertex_id, info|
+$vertices.each do |vertex_id, info|
 	info[:edges].each do |edge|
-		reverse_vertex = vertices_reverse[edge]
+		reverse_vertex = $vertices_reverse[edge]
 
 		if reverse_vertex
 			reverse_vertex[:edges] << vertex_id
 		else
-			vertices_reverse[edge] = { seen: false, edges: [vertex_id] }
+			$vertices_reverse[edge] = { seen: false, edges: [vertex_id] }
 		end
 	end
 end
 
+def mark_seen(vertex)
+	vertex[:seen] = true
+end
 
 
-# Build a visible graph
+def topological_sort
+	$vertices.each do |vertex_id, info|
+		vertex = $vertices[vertex_id]
+		if !vertex[:seen]
+			dfs_visit_sort(vertex, vertex_id)
+		end
+	end
+end
 
-# seen = {
-# 	1: false,
-# 	2: false
-# }
+def dfs_visit(vertex, vertex_id)
+	mark_seen(vertex)
 
-# $sort = []
+	$scc << vertex_id
 
-# def topological_sort
-# 	vertices.each do |vertex_id, edges|
-# 		if !seen[vertex_is]
-# 			dfs_visit_sort(vertex_id)
-# 		end
-# 	end
-# end
+	vertex[:edges].each do |e|
+		edge = $vertices_reverse[e]
+		if !edge[:seen]
+			dfs_visit(edge, e)
+		end
+	end
+end
 
-# def dfs_visit_sort(vertex_id)
-# 	seen[vertex_id] = true
+# Pass vertex_id so that I do not have to repeat the look up
+def dfs_visit_sort(vertex, vertex_id)
+	mark_seen(vertex)
+	
+	vertex[:edges].each do |e|
+		edge = $vertices[e]
 
-# 	vertices[vertex_id].each do |edge_id|
-# 		if !seen[edge_id]
-# 			dfs_visit_sort(edge_id)
-# 		end
-# 	end
+		if !edge[:seen]
+			dfs_visit_sort(edge, e)
+		end
+	end
 
-# 	$sort << vertex_id
-# end
+	$sort << vertex_id
+end
 
+
+topological_sort
+
+while !$sort.empty? do
+	vertex_id = $sort.pop
+	vertex = $vertices_reverse[vertex_id]
+
+	if !vertex[:seen]
+		dfs_visit(vertex, vertex_id)
+		$all_scc << $scc
+		$scc = []
+	end
+end
+
+p $all_scc.map(&:size).sort
 
 
 
